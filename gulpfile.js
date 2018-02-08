9a406101435cb9d032250e568e7712a414bb3cb5
idleberg <jan@idleberg.com>
2018-02-08 16:13:22 +0100

    simplify

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ const svgFiles = [
 ];
 
 // Lint TypeScript
-gulp.task('lint:ts', gulp.series( (done) => {
+gulp.task('lint:ts', (done) => {
   gulp.src(tsFiles)
     .pipe(debug({title: 'tslint'}))
     .pipe(tslint({
@@ -43,34 +43,34 @@ gulp.task('lint:ts', gulp.series( (done) => {
     }))
     .pipe(tslint.report())
   done();
-}));
+});
 
 // Lint JSON
-gulp.task('lint:json', gulp.series( (done) => {
+gulp.task('lint:json', (done) => {
   gulp.src(jsonFiles)
     .pipe(debug({title: 'json-lint'}))
     .pipe(jsonlint())
     .pipe(jsonlint.failAfterError())
     .pipe(jsonlint.reporter());
   done();
-}));
+});
 
 // Validate XML
-gulp.task('lint:xml', gulp.series( (done) => {
+gulp.task('lint:xml', (done) => {
   gulp.src(xmlFiles)
     .pipe(debug({title: 'xml-validator'}))
     .pipe(xmlVal());
   done();
-}));
+});
 
 // Convert SVG
-gulp.task('convert:svg', gulp.series( (done) => {
+gulp.task('convert:svg', (done) => {
   gulp.src(svgFiles)
     .pipe(raster())
     .pipe(rename("logo.png"))
     .pipe(gulp.dest('./images'));
   done();
-}));
+});
 
 // Available tasks
 gulp.task('lint', gulp.parallel('lint:ts', 'lint:json', 'lint:xml', (done) => {
