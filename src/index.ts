cd03cd9402a6a0042bceb7f880814c021adc505e
Jan T. Sott <git@idleberg.com>
2024-04-02 23:43:26 +0200

    remove telemetry

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 'use strict';
 
 // Dependencies
-import { commands, env, type ExtensionContext } from 'vscode';
+import { commands, type ExtensionContext } from 'vscode';
 
 // Package Components
 import { createTask} from './task';
 import { generate } from './pynsist';
-import { getConfig } from 'vscode-get-config';
-import { reporter } from './telemetry';
 
 async function activate(context: ExtensionContext): Promise<void> {
-  const { disableTelemetry } = await getConfig('pynsist');
-
-  if (env.appName !== 'VSCodium' && disableTelemetry === false) {
-    context.subscriptions.push(reporter);
-  }
-
 	context.subscriptions.push(
 		commands.registerTextEditorCommand('extension.pynsist.generate', () => {
 			generate(false);
